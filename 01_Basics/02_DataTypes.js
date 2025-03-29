e52826d0bd92a0ece003dce58f366e6cf400773b
raopremrao <raom61337@gmail.com>
2025-03-29 23:02:27 +0530

    Done by T Prem

--- 01_Basics/02_DataTypes.js
+++ 01_Basics/02_DataTypes.js
@@ -3,5 +3,28 @@
 // alert(3+3) //we are using nodejs, not browser
 
 console.log(3+3) 
-
+ 
 console.log("Prem")
+
+let name ="Prem"
+let age =18
+let isLoggedIn = false
+let state
+
+
+// number => 2 to power of 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
