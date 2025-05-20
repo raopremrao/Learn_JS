c08086275a8a3336519f629c61141fd61b4d2b74
raopremrao <raom61337@gmail.com>
2025-05-20 16:20:09 +0530

    Hello World

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -33,10 +33,33 @@ let stringNumber = String(someNumber)
 
 let value = 3
 let negValue = -value
-console.log(negValue);
+// console.log(negValue);
 
-console.log(2+2);
-console.log(2-2);
-console.log(2*2);
-console.log(2**2);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+
+let str1= "Prem"
+let str2 = " Prem"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+
+// console.log("1" + 2 );
+// console.log(1 + "2" );
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2");
+// console.log(1 + 2 + "2" + 1 + 1)
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(true +);
+// its an error  
+
+
+
+
 
