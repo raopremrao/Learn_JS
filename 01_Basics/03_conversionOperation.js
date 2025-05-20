d47ef7397b13fc5e7aa6c454f1d90b555f20044b
raopremrao <raom61337@gmail.com>
2025-05-20 14:50:06 +0530

    Hello World

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,4 +1,42 @@
-let score = 33
+let score = true
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+
+//"33" => 33
+// "33abc" => NaN
+//true => 1
+//undefined => NaN
+// null => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1=> true ; 0: false
+// "" => false
+//"Prem" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+
+// ***************  Operations **********
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+
