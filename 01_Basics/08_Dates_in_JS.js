42bcdbf561b2f3fbdbb80bd93c95e017d0572e18
raopremrao <raom61337@gmail.com>
2025-06-03 13:44:43 +0530

    Date and Time Done

---
+++ 01_Basics/08_Dates_in_JS.js
@@ -0,0 +1,10 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString()); // Current date and time
+
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString()); 
+
+console.log(typeof myDate); // object
+
