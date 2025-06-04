484a833ec8e232a1bddec52c422f6dda24139255
raopremrao <raom61337@gmail.com>
2025-06-04 17:43:56 +0530

    Doing objects in js

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,20 @@
+// singleton
+
+
+
+// object literals
+const JsUser = {
+    name: "Prem",
+    "full name": "T Prem",
+    age: 18,
+    locaation: "India",
+    email: "raom61337@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+
+
+
