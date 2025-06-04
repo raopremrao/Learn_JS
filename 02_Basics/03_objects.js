ca68398b60a87f7bd20bd7e860e4a0ca34e4268d
raopremrao <raom61337@gmail.com>
2025-06-04 20:34:16 +0530

    Still doing Objects

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -3,9 +3,17 @@
 
 
 // object literals
+
+
+const mySym = Symbol("Key1");
+
+
+
+
 const JsUser = {
     name: "Prem",
     "full name": "T Prem",
+    [mySym]: "mykey1",
     age: 18,
     locaation: "India",
     email: "raom61337@gmail.com",
@@ -13,8 +21,23 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]); // it will work
+// console.log(JsUser.full name); // it will not work, because of space in the key
+// console.log(JsUser.[mySym]);
+
+JsUser.email = "tm74rao@gmail.com"
+// Object.freeze(JsUser); // it will not allow to change the object
+// JsUser.email = "raoom61337@gmail.com" // it will not change the email because the object is frozen
+// console.log(JsUser);
+
+JsUser.greeting = function () {
+    console.log("Hello JS User");
+}
+
+console.log(JsUser.greeting());
+
 
 
 
---
+++ 02_Basics/04_Objects.js
@@ -0,0 +1,61 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+tinderUser.id = "123abc"
+tinderUser.name = "T Prem"
+tinderUser.isLoggedIn = false
+
+const regularUser = {
+    email: "Some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "T",
+            lastname: "Prem"}
+    }
+
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+// console.log(tinderUser);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+const obj3 = {...obj1, ...obj2}
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+// console.log(obj3);
+// console.log(obj1); 
+
+const users = [
+    {
+        id: 1,
+        email: "hello@gmail.com"
+    },
+    {
+        id: 2,
+        email: "hello2@gmail.com"
+    },
+    {
+        id: 3,
+        email: "hello3@gmail.com"
+    }
+]
+
+// users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser)); // Returns an array of keys
+console.log(Object.values(tinderUser)); // Returns an array of values
+console.log(Object.entries(tinderUser)); // Returns an array of key-value pairs in the form of arrays in Array
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // Returns true if the object has the property, otherwise false
+
+
+
+
+
+
+
+
+
