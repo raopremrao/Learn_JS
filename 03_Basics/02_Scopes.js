a5cb7fe2396eea3a9236ca1033b81779f21554bf
raopremrao <raom61337@gmail.com>
2025-06-11 10:58:54 +0530

    Done with scopes

---
+++ 03_Basics/02_Scopes.js
@@ -0,0 +1,20 @@
+// let a = 10
+// const b = 20
+// var c = 30
+// var c = 300
+let a = 100
+
+if (true){
+    let a = 10
+    const b = 20
+    console.log("Inner:", a);
+    
+    // var c = 30
+}
+
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
