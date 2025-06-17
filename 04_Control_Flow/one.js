54bbe9719b384bb1888c131d25b42281312cf26f
raopremrao <raom61337@gmail.com>
2025-06-17 13:59:25 +0530

    Starting DOM

---
+++ 04_Control_Flow/one.js
@@ -0,0 +1,22 @@
+// if
+
+
+// if (condition){
+//     // code to be executed if condition is true
+// }
+
+const isUserLoggedIn  = true
+const temperature = 40
+
+if ( 2 == "2" ){
+    console.log("The condition is true"); 
+
+}
+if ( temperature > 50){
+    console.log("greater than 50");
+    
+}
+else {
+    console.log("temperature is less than 50");
+}
+// console.log("temperature is greater than 50");
